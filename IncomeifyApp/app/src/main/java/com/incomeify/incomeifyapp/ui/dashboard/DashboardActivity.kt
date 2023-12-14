435d839fc008caccd4021f682a2ba9daa08d1302
Rama Danudinata <danunata45@gmail.com>
2023-12-14 15:59:53 +0700

    testing auth branch

--- IncomeifyApp/app/src/main/java/com/incomeify/incomeifyapp/ui/dashboard/DashboardActivity.kt
+++ IncomeifyApp/app/src/main/java/com/incomeify/incomeifyapp/ui/dashboard/DashboardActivity.kt
@@ -8,6 +8,7 @@ class DashboardActivity : AppCompatActivity() {
 
     // testing commit
     // testing commit 2
+    // testinc auth branch commit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
