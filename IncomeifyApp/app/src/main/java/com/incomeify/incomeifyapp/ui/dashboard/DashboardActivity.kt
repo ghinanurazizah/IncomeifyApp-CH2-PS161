d0b6e25e4b7e51396d57d1830ddbf02dc43dc3ca
Rama Danudinata <2110511096@mahasiswa.upnvj.ac.id>
2023-12-14 15:09:47 +0700

    testing 1 commit

--- IncomeifyApp/app/src/main/java/com/incomeify/incomeifyapp/ui/dashboard/DashboardActivity.kt
+++ IncomeifyApp/app/src/main/java/com/incomeify/incomeifyapp/ui/dashboard/DashboardActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import com.incomeify.incomeifyapp.R
 
 class DashboardActivity : AppCompatActivity() {
+
+    // testing commit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
