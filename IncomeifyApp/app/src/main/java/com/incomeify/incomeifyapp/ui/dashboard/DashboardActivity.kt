9306203707263c48232bd669a86cecd927438e3b
Rama Danudinata <89115897+danudinataa@users.noreply.github.com>
2023-12-14 14:59:50 +0700

    Add files via upload

---
+++ IncomeifyApp/app/src/main/java/com/incomeify/incomeifyapp/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.incomeify.incomeifyapp.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.incomeify.incomeifyapp.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
